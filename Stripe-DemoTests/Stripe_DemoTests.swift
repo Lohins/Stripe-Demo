9187951f77aedb8f6285c1e28eb68a70f538dc8b
Lohins <comeonsitong@gmail.com>
2016-08-11 09:44:36 -0300

    Initial Commit

---
+++ Stripe-DemoTests/Stripe_DemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  Stripe_DemoTests.swift
+//  Stripe-DemoTests
+//
+//  Created by S.t on 16/8/11.
+//  Copyright © 2016年 S.t. All rights reserved.
+//
+
+import XCTest
+@testable import Stripe_Demo
+
+class Stripe_DemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
